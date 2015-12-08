1eb5c545679f5c5ebaad0d8062e2f5260add6c1a
Fred Cons <frederic.cons@gmail.com>
2015-12-08 23:06:47 +0100

    search wip

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -5,6 +5,7 @@ import com.typesafe.config.ConfigFactory
 import models.Document
 import models.UserSearch
 import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms
 import play.api._
 import play.api.mvc._
 import play.api.data._
@@ -16,6 +17,8 @@ import play.api.i18n.Messages.Implicits._
 import com.sksamuel.elastic4s.jackson.ElasticJackson.Implicits._
 
 import models.SearchImplicits._
+
+import scala.collection.mutable
 
 object Search extends Controller {
 
@@ -48,8 +51,18 @@ object Search extends Controller {
         /* binding success, you get the actual value. */
         val userSearch = UserSearch(userData.keyword)
         import com.sksamuel.elastic4s.ElasticDsl._
-        val response = client.execute { search in "documents"->"title" query userSearch.keyword}.await
+        val response = client.execute {
+          search in "afnor" types "documents" fields("titlefr", "summary") query userSearch.keyword aggregations {
+            aggregation terms "orgori" field "orgori" size 5
+            nestedAggregation("supports").path("supports").aggregations(
+              aggregation terms "format" field "format"
+            )
+          }
+        }.await
         val documents = response.as[Document]
+        val aggOrgOri = response.aggregations.getAsMap.get("orgori").asInstanceOf[StringTerms]
+        //agg.getBuckets.size shouldBe 5
+        //agg.getBucketByKey("foo").getDocCount
         Ok(views.html.search(searchForm, documents))
       }
     )
